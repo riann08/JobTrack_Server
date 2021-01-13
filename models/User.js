040e41b3bab10db0291b695c78fc287785414467
Ann Lampa <riann08@yahoo.com>
2021-01-13 14:51:32 +0100

    add Models

--- models/User.js
+++ models/User.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePhoto: String,
+    jobs: [{ type: Schema.Types.ObjectId, ref: "Job" }],
+  },
+  {
+    timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
 module.exports = User;
