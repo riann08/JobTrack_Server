7719561894ba48b645a95ec2fcc4f4b642b65097
Ann Lampa <riann08@yahoo.com>
2021-01-22 08:15:11 +0100

    protect routes

--- routes/job.js
+++ routes/job.js
@@ -3,27 +3,23 @@ const router = new express.Router();
 const Job = require("../models/Job");
 const User = require("../models/User");
 const upload = require("./../config/cloudinary");
-//const requireAuth = require("../middlewares/requireAuth");
+const requireAuth = require("../middlewares/requireAuth");
 
 //http:localhost:4000/api/job (READ)
-router.get(
-  "/",
-  //requireAuth,
-  (req, res, next) => {
-    Job.find()
-      .then((jobs) => {
-        res.status(200).json(jobs);
-      })
-      .catch((error) => {
-        next(error);
-      });
-  }
-);
+router.get("/", requireAuth, (req, res, next) => {
+  Job.find()
+    .then((jobs) => {
+      res.status(200).json(jobs);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 //http:localhost:4000/api/job (CREATE)
 router.post(
   "/",
-  //requireAuth,
+  requireAuth,
   //upload.single("profileImg"),
   async (req, res, next) => {
     console.log(req.body);
@@ -50,7 +46,7 @@ router.post(
 //http:localhost:4000/api/job/:id (UPDATE)
 router.patch(
   "/:id",
-  //requireAuth,
+  requireAuth,
   //upload.single("profileImg"),
   function (req, res, next) {
     Job.findByIdAndUpdate(req.params.id, req.body, { new: true })
@@ -64,50 +60,38 @@ router.patch(
 );
 
 //http:localhost:4000/api/job (DELETE)
-router.delete(
-  "/:id",
-  //requireAuth,
-  function (req, res, next) {
-    Job.findByIdAndDelete(req.params.id)
-      .then((respondApi) => {
-        res.status(200).send("Job application successfully deleted!");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-);
+router.delete("/:id", requireAuth, function (req, res, next) {
+  Job.findByIdAndDelete(req.params.id)
+    .then((respondApi) => {
+      res.status(200).send("Job application successfully deleted!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 //search bar find by company name
 //http:localhost:4000/api/search
 
-router.get(
-  "/search",
-  //requireAuth,
-  function (req, res, next) {
-    Job.find({ company: "IBM" })
-      .then((job) => {
-        res.status(200).json(job);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-);
+router.get("/search", requireAuth, function (req, res, next) {
+  Job.find({ company: "IBM" })
+    .then((job) => {
+      res.status(200).json(job);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 //http:localhost:4000/api/job/:id (READ)
-router.get(
-  "/:id",
-  //requireAuth,
-  (req, res, next) => {
-    Job.findById(req.params.id)
-      .then((job) => {
-        res.status(200).json(job);
-      })
-      .catch((error) => {
-        next(error);
-      });
-  }
-);
+router.get("/:id", requireAuth, (req, res, next) => {
+  Job.findById(req.params.id)
+    .then((job) => {
+      res.status(200).json(job);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,58 +2,45 @@ const express = require("express");
 const router = new express.Router();
 const User = require("../models/User");
 const upload = require("./../config/cloudinary");
-//const requireAuth = require("../middlewares/requireAuth");
+const requireAuth = require("../middlewares/requireAuth");
 
 //http:localhost:4000/api/user/ (READ)
-router.get(
-  "/",
-  //requireAuth,
-  (req, res, next) => {
-    User.find()
-      .then((users) => {
-        res.status(200).json(users);
-      })
-      .catch((error) => {
-        next(error);
-      });
-  }
-);
+router.get("/", requireAuth, (req, res, next) => {
+  User.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 //http:localhost:4000/api/user (CREATE)
-router.post(
-  "/",
-  //requireAuth,
-  upload.single("profileImg"),
-  (req, res, next) => {
-    User.create(req.body)
-      .then((newUserAdded) => {
-        res.status(201).json(newUserAdded);
-      })
-      .catch((error) => {
-        next(error);
-      });
-  }
-);
+router.post("/", requireAuth, upload.single("profileImg"), (req, res, next) => {
+  User.create(req.body)
+    .then((newUserAdded) => {
+      res.status(201).json(newUserAdded);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 //http:localhost:4000/api/user/:id (READ)
-router.get(
-  "/:id",
-  //requireAuth,
-  (req, res, next) => {
-    User.findById(req.params.id)
-      .then((user) => {
-        res.status(200).json(user);
-      })
-      .catch((error) => {
-        next(error);
-      });
-  }
-);
+router.get("/:id", requireAuth, (req, res, next) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 //http:localhost:4000/api/user (UPDATE)
 router.patch(
   "/:id",
-  //requireAuth,
+  requireAuth,
   //upload.single("profileImg"),
   function (req, res, next) {
     User.findByIdAndUpdate(req.params.id, req.body, { new: true })
@@ -67,19 +54,15 @@ router.patch(
 );
 
 //http:localhost:4000/api/user (DELETE)
-router.delete(
-  "/me",
-  //requireAuth,
-  function (req, res, next) {
-    console.log("here" + req.session.currentUser);
-    User.findOneAndDelete(req.session.currentUser)
-      .then((respondApi) => {
-        res.status(200).send("User successfully deleted!");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-);
+router.delete("/me", requireAuth, function (req, res, next) {
+  console.log("here" + req.session.currentUser);
+  User.findOneAndDelete(req.session.currentUser)
+    .then((respondApi) => {
+      res.status(200).send("User successfully deleted!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
